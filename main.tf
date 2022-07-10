c999e02750c4236c095b1a9e191f0574725da5fd
singhgk420 <73923943+singhgk420@users.noreply.github.com>
2022-07-10 11:07:57 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -14,7 +14,8 @@ data "aws_ami" "ubuntu" {
     values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
   }
   }
- // provider "aws" {
-
- // region = "us-east-1"
-//}
+ provider "aws" {
+  region = "us-east-1"
+   shared_credentials_file = "~/.aws/credentials"
+   profile = "testing"
+}
