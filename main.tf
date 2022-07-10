372653be3f3bd15632679d913bd7987f20f49671
singhgk420 <73923943+singhgk420@users.noreply.github.com>
2022-07-10 11:52:58 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -15,7 +15,7 @@ data "aws_ami" "ubuntu" {
   }
   }
  provider "aws" {
-  region = "us-east-1"
+  region = "us-west-1"
    access_key = "AKIAQQSWUB4XYJXRP2FB"
   secret_key = "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"
 
