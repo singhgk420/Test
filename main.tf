77e5f3cd85e86d027af2f2c148675c4426d66c73
singhgk420 <xogauravsinghxo@gmail.com>
2022-07-10 09:54:18 +0530

    adding main file

---
+++ main.tf
@@ -0,0 +1,19 @@
+resource "aws_instance" "web" {
+  ami           = data.aws_ami.ubuntu.id
+  instance_type = "t3.micro"
+
+  tags = {
+    Name = "HelloWorld"
+  }
+}
+data "aws_ami" "ubuntu" {
+  most_recent = true
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
+  }
+  }
+  provider "aws" {
+  region = "us-east-1"
+}
