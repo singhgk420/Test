002f4ab6ae7d9819225c23505994539811b8d4d2
singhgk420 <xogauravsinghxo@gmail.com>
2022-07-11 16:24:06 +0530

    adding var file

--- main.tf
+++ main.tf
@@ -15,6 +15,8 @@ data "aws_ami" "ubuntu" {
   }
   }
   provider "aws" {
+     access_key = "AKIAQQSWUB4XTDM5UIUI"
+     secret_key = "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"  
 
   region = "us-east-1"
 }
---
+++ var.tf
@@ -0,0 +1,6 @@
+variable "access_key"{
+    default="AKIAQQSWUB4XSNUWGXIE"
+}
+variable "secret_key" {
+    default ="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"
+}
