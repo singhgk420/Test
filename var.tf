909bd37b49ffc192ab222f3d4a500813e4334a15
singhgk420 <73923943+singhgk420@users.noreply.github.com>
2022-07-12 09:05:20 +0530

    Update var.tf

--- var.tf
+++ var.tf
@@ -1,6 +1,6 @@
-variable "access_key"{
-    default="AKIAQQSWUB4X5TC5QVUR"
-}
-variable "secret_key" {
-    default ="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"
-}
+//variable "access_key"{
+    //default="AKIAQQSWUB4X5TC5QVUR"
+//}
+////variable "secret_key" {
+    //default ="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"
+//}
